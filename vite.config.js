ef68d15691c419972a77a2f8f4f59b097601c5fe
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-07-04 16:05:19 +0545

    update package json and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.jsx'),
+      name: 'FreshPkg',
+      fileName: (format) => `fresh-pkg.${format}.js`,
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
